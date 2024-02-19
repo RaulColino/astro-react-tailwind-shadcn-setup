535338b1ced9e1ba392af3025a288e2b844054bc
rcs <r.colinos.2016@alumnos.urjc.es>
2024-02-19 20:36:59 +0100

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./components/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./app/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", ...fontFamily.sans], //default font-family
+        dmsans: ["DM Sans", ...fontFamily.sans],
+        urbanist: ["Urbanist", ...fontFamily.sans],
+        poppins: ["Poppins", ...fontFamily.sans],
+        jakarta: ["Plus Jakarta Sans", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
